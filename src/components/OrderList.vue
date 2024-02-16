a067e11762395a304067a36627c1d18dec87bbe5
Kamaboko0126 <4b232011@gm.student.ncut.edu.tw>
2024-02-16 10:15:47 +0800

    20240213

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -51,8 +51,25 @@ export default {
       arrayOrder.value = response.data;
     };
 
+    const finished = async (id) => {
+      console.log(id);
+      try {
+        const response = await axios.put(
+          `http://127.0.0.1:10000/orderlist/finish/${id}`
+        );
+        // console.log(response)
+        if (response.data.message === "success") {
+          console.log("success");
+          getOrderData();
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
     return {
       arrayOrder,
+      finished,
     };
   },
 };
@@ -60,7 +77,57 @@ export default {
 
 <template>
   <div class="cards">
-    <div class="card" v-for="order in arrayOrder" :key="order.id">
+    <v-draggable v-model="arrayOrder" tag="ul" :disabled="false" itemKey="id">
+      <template #item="{ element: order }">
+        <div class="card" v-if="!order.is_finished">
+          <div class="card-top">
+            <div class="number">{{ order.index_id + " 號" }}</div>
+            <i class="material-icons" @click="finished(order.order_id)"
+              >close</i
+            >
+          </div>
+          <div class="infos">
+            <!-- <h3>{{ order.is_discount ? "員工價：是" : "員工價：否" }}</h3> -->
+            <h3>{{ "訂購方式：" + order.ordering_method }}</h3>
+            <h3>{{ "訂購時間：none" }}</h3>
+            <!-- <h3>{{ "付款狀態：" + order.payment }}</h3> -->
+            <!-- <h3>{{ order.phone == "09-XXXXXXXX" ? "" : order.phone }}</h3> -->
+            <!-- <h3>{{ order.is_finished ? "結單狀態：是" : "結單狀態：否" }}</h3> -->
+          </div>
+
+          <div class="products">
+            <v-draggable
+              v-model="order.lists"
+              tag="ul"
+              :disabled="false"
+              itemKey="id"
+            >
+              <template #item="{ element: list }">
+                <div class="list">
+                  <div class="product">
+                    <div>{{ list.name }}</div>
+                    <div>{{ list.quantity }}</div>
+                  </div>
+
+                  <div class="markers">
+                    <div
+                      v-for="marker in list.markers.filter(
+                        (marker) => marker.checked
+                      )"
+                      :key="marker.id"
+                      class="marker"
+                    >
+                      {{ marker.value }},
+                    </div>
+                  </div>
+                </div>
+              </template>
+            </v-draggable>
+          </div>
+        </div>
+      </template>
+    </v-draggable>
+    <!-- <div class="card" v-for="order in arrayOrder" :key="order.id">
       <h3>{{ "訂單編號：" + order.index_id }}</h3>
       <h3>{{ order.is_discount ? "員工價：是" : "員工價：否" }}</h3>
       <h3>{{ "訂購方式：" + order.ordering_method }}</h3>
@@ -69,20 +136,112 @@ export default {
       <h3>{{ order.phone == "09-XXXXXXXX" ? "" : order.phone }}</h3>
       <h3>{{ order.is_finished ? "結單狀態：是" : "結單狀態：否" }}</h3>
       <div v-for="list in order.lists" :key="list.id">
-        <h2><span>{{ list.name }}</span><span>{{ list.quantity }}</span></h2>
+        <h2>
+          <span>{{ list.name }}</span
+          ><span>{{ list.quantity }}</span>
+        </h2>
         <h3>
-          <span v-for="marker in list.markers.filter(marker => marker.checked)" :key="marker.id">
+          <span
+            v-for="marker in list.markers.filter((marker) => marker.checked)"
+            :key="marker.id"
+          >
             {{ marker.value }}<span>,</span>
           </span>
         </h3>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <style scoped>
-  .cards{
-    display: flex;
-    flex-wrap: wrap;
-  }
+.cards {
+  height: calc(100vh - var(--header-height));
+  width: 100%;
+  padding: 10px 20px;
+  display: flex;
+  align-items: start;
+  justify-content: start;
+  flex-wrap: wrap;
+  overflow: auto;
+}
+
+.cards ul {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.card {
+  width: 300px;
+  /* height: 300px; */
+  background: #fff;
+  box-shadow: 0 10px 20px -12px rgba(0, 0, 0, 0.42),
+    0 3px 20px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);
+  border-bottom: 1px solid rgba(153, 153, 153, 0.3);
+  margin: 15px;
+  /* cursor: pointer; */
+}
+
+.card-top {
+  border-radius: 4px 4px 0 0;
+  background: var(--header-color);
+  color: #ffffff;
+  padding: 10px 25px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.card-top i {
+  font-size: 30px;
+  padding: 1.5px;
+  cursor: pointer;
+  background: var(--cancel-color);
+  border-radius: 50px;
+}
+
+.number {
+  font-size: 23px;
+}
+
+.infos {
+  padding: 5px 15px;
+  border-bottom: 1px solid var(--header-color);
+}
+
+.products {
+  padding: 5px 15px;
+  width: 100%;
+}
+
+/* lists */
+.products ul {
+  flex-wrap: nowrap;
+  flex-direction: column;
+  padding-bottom: 10px;
+}
+
+.list {
+  display: flex;
+  flex-direction: column;
+  padding: 7px 10px 7px 0;
+  font-size: 28px;
+  /* cursor: pointer; */
+}
+
+.product {
+  display: flex;
+  justify-content: space-between;
+  font-size: 25px;
+}
+
+.markers {
+  width: 100%;
+  display: flex;
+}
+
+.marker {
+  font-size: 20px;
+  margin: 0 7px 0 0;
+  padding: 0;
+}
 </style>
